3f708d8f213c28a2bc7a8d9936885ccc95d0289b
lily <lilysht50@gmail.com>
2019-02-09 14:12:48 +0700

    - Perubahan konfigurasi wordpress

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'rev_wpci');
+define('DB_NAME', '');
 
 /** MySQL database username */
 define('DB_USER', 'root');
